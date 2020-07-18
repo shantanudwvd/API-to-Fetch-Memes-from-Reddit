711487bc0b234bfe92ca417bbc6ef38bf943ffb3
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2020-07-18 18:29:20 +0530

    Updated the app file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import random, logging
-from Main import get_meme, check_image, get_text
+from main import get_meme, check_image, get_text
 
 app = Flask(__name__)
 count = 0
